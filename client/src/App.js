8c70087f1ff77c9520da0d32ad8aa5f39135945b
“SanjanaBandara” <sanjana.bandara@work.begood.solutions>
2023-04-21 00:44:43 +0530

    feat(): implemented the description box and button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ description });
+    setDescription("");
+  };
+
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className="app">
+      <h1>Website Idea Generator</h1>
+      <form method="POST" onSubmit={handleSubmit}>
+        <label htmlFor="description">Enter the description</label>
+        <textarea
+          name="description"
+          rows={6}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button>GENERATE</button>
+      </form>
     </div>
   );
 }
